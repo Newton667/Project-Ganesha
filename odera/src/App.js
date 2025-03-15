ba94071527ecc5d8934659896975bf861ed466ca
anishb788 <anishboddu68905@gmail.com>
2025-03-15 01:33:36 -0400

    Working navbar, sites not loading, will pick up later

---
+++ odera/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="content">
+        <Routes>
+          <Route exact path ="/" component={<Home />} />
+          <Route exact path ="/about" component={<About />} />
+          <Route exact path ="/contact" component={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ odera/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+    return(
+        <div>
+            <h1>Contact Us</h1>
+            <p>308 Negra Arroyo Lane, Albuquerque, New Mexico 87104 </p>
+        </div>
+    );
+};
+
+export default Contact;

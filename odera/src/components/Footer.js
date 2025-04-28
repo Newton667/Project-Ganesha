36684d1bd55fc79a866d5ff2c90a4f06d057dcad
anishb788 <anishboddu68905@gmail.com>
2025-04-28 13:17:39 -0400

    Completed prototype footer, need to add CSS and flesh out links

--- odera/src/components/Footer.js
+++ odera/src/components/Footer.js
@@ -6,14 +6,49 @@ function Footer() {
         <footer style={{backgroundColor: 'f8f8f8', padding: '2rem', marginTop: '2rem', borderTop: '1px solid #ccc'}}>
             <div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap'}}>
                 
+                <div>
+                    <h1>Odera</h1>
+                </div>
+
+
+                {/* Jobs section */}
+                <div>
+                    <h4>Jobs</h4>
+                    <ul style={{listStyleType: 'none', padding: '0'}}>
+                        <li><Link to="/about">Catagories</Link></li>
+                        <li><Link to="/about">Projects</Link></li>
+                        <li><Link to="/about">Companies</Link></li>
+                        <li><Link to="/about">Freelancers</Link></li>
+                    </ul>
+                </div>
+
+                {/* Contact section */}
+                <div>
+                    <h4>About</h4>
+                    <ul style={{listStyleType: 'none', padding: '0'}}>
+                        <li><Link to="/about">Email</Link></li>
+                        <li><Link to="/about">LinkedIn</Link></li>
+                        <li><Link to="/about">Twitter</Link></li>
+                        <li><Link to="/about">Instagram</Link></li>
+                    </ul>
+                </div>
+
                 {/* Example section */}
                 <div>
                     <h4>About</h4>
                     <ul style={{listStyleType: 'none', padding: '0'}}>
+                        <li><Link to="/about">Home</Link></li>
                         <li><Link to="/about">About Us</Link></li>
-                        <li><Link to="/about">About Us</Link></li>
-                        <li><Link to="/about">About Us</Link></li>
+                        <li><Link to="/about">Our Mission</Link></li>
+                        <li><Link to="/about">Our Partners</Link></li>
                     </ul>
+                </div>
+
+                <div>
+                    <h4>Join our newsletter!</h4>
+                    <label for="email">Email address:</label>
+                    <input type="text" id="email" name="email"></input>
+                    <input type="submit" value="Submit"></input>
                 </div>
 
 
